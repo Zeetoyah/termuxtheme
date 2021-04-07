41ed2a25eba2d6e6434d6775594102dc72d20bd4
Zeetoyah <82073700+Zeetoyah@users.noreply.github.com>
2021-04-07 23:42:47 +0700

    Add files via upload

---
+++ tema.sh
@@ -0,0 +1,16 @@
+apt update && apt upgrade
+apt install git
+git clone https://github.com/Cabbagec/termux-ohmyzsh
+pkg install figlet
+figlet ZeeChrist
+pkg install sl
+echo
+figlet ada kereta
+figlet lewat gan
+sl
+echo
+echo
+echo "note:"
+echo "ketik (cd termux-ohmyzsh)"
+echo "silahkan masukan (sh install.sh) untuk memulai"
+echo "script by ZeeChrist"
