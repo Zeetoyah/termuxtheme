a6c88bab495c3f15b6ec6b55c70be82dd315e116
Zeetoyah <82073700+Zeetoyah@users.noreply.github.com>
2021-04-07 22:19:25 +0700

    Add files via upload

---
+++ tema.sh
@@ -0,0 +1,11 @@
+apt update && apt upgrade
+apt install git
+git clone https://github.com/Cabbagec/termux-ohmyzsh
+pkg install figlet
+figlet ZeeChrist
+echo
+echo
+echo "note:"
+echo "ketik (cd termux-ohmyzsh)"
+echo "silahkan masukan (sh install.sh) untuk memulai"
+echo "script by ZeeChrist"
---
+++ zeetheme.sh
@@ -0,0 +1,17 @@
+apt update && apt upgrade
+pkg install figlet
+figlet By:ZeeChrist
+pkg install nano
+pkg install ruby
+pkg install cowsay
+pkg install figlet
+pkg install toilet
+gem install lolcat
+nano /data/data/com.termux/files/usr/etc/profile
+clear
+
+echo "cowsay -f eyes "Zeevalya Christiano" | lolcat
+toilet -f standard "Amel-chan" -F gay
+neofetch
+date | lolcat"
+echo "tekan ctrl+x lalu y"
